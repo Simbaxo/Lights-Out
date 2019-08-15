821ebd59b01f30890e8840c4a25f5f04d107c457
Andres Mena <ballerlikemike23@gmail.com>
2019-08-14 21:09:30 -0400

    Displaying the Game Board

--- src/Board.js
+++ src/Board.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Cell from "./Cell";
 import './Board.css';
 
@@ -30,13 +30,19 @@ import './Board.css';
  **/
 
 class Board extends Component {
+  static defaultProps = {
+    nrows: 5,
+    ncols: 5,
+    chanceLightStartsOn: 0.25
+  }
 
   constructor(props) {
     super(props);
 
     // TODO: set initial state
-    state = {
-
+    this.state = {
+      hasWon: false,
+      board: this.createBoard()
     }
   }
 
@@ -45,13 +51,20 @@ class Board extends Component {
   createBoard() {
     let board = [];
     // TODO: create array-of-arrays of true/false values
+    for (let y = 0; y < this.props.nrows; y++) {
+      let row = []
+      for (let x = 0; x < this.props.ncols; x++) {
+        row.push(Math.random() < this.props.chanceLightStartsOn)
+      }
+      board.push(row)
+    }
     return board
   }
 
   /** handle changing a cell: update board & determine if winner */
 
   flipCellsAround(coord) {
-    let {ncols, nrows} = this.props;
+    let { ncols, nrows } = this.props;
     let board = this.state.board;
     let [y, x] = coord.split("-").map(Number);
 
@@ -69,18 +82,13 @@ class Board extends Component {
     // win when every cell is turned off
     // TODO: determine is the game has been won
 
-    this.setState({board, hasWon});
+    this.setState({ board });
   }
 
 
   /** Render game board or winning message. */
 
   render() {
-
-    <div>
-
-    </div>
-
     // if the game is won, just show a winning msg & render nothing else
 
     // TODO
@@ -88,6 +96,24 @@ class Board extends Component {
     // make table board
 
     // TODO
+
+    let tblBoard = []
+    for (let y = 0; y < this.props.nrows; y++) {
+      let row = []
+      for (let x = 0; x < this.props.ncols; x++) {
+        let coord = `${y}-${x}`
+        row.push(<Cell key={coord} isLit={this.state.board[y][x]} />)
+      }
+      tblBoard.push(<tr key={y}>{row}</tr>)
+    }
+    return (
+      <table className="Board">
+        <tbody>
+          {tblBoard}
+        </tbody>
+      </table>
+    )
+
   }
 }
 
