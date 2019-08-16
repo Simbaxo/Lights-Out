e6d4d3c3f44dafa2118b4f45a7239d79a12850cb
Andres Mena <ballerlikemike23@gmail.com>
2019-08-15 23:11:02 -0400

    Styling the Game

--- src/Board.js
+++ src/Board.js
@@ -78,7 +78,7 @@ class Board extends Component {
     }
     // TODO: flip this cell and the cells around it
     flipCell(y, x) // Flip initial cell
-    flipCell(y, x -1) // flip left
+    flipCell(y, x - 1) // flip left
     flipCell(y, x + 1) // flip right
     flipCell(y - 1, x) // flip below
     flipCell(y + 1, x) // flip above
@@ -95,7 +95,14 @@ class Board extends Component {
 
   render() {
     if (this.state.hasWon) {
-      return <h1>YOU WON!!!</h1>
+      return (
+        <div className="Board-title">
+          <div className="winner">
+          <span className="neon-orange">YOU</span>
+          <span className="neon-blue">WIN</span>
+          </div>
+        </div>
+      )
     }
     // if the game is won, just show a winning msg & render nothing else
     // TODO
@@ -112,11 +119,17 @@ class Board extends Component {
       tblBoard.push(<tr key={y}>{row}</tr>)
     }
     return (
-      <table className="Board">
-        <tbody>
-          {tblBoard}
-        </tbody>
-      </table>
+      <div>
+        <div className="Board-title">
+          <div className="neon-orange">Lights</div>
+          <div className="neon-blue">Out</div>
+        </div>
+        <table className="Board">
+          <tbody>
+            {tblBoard}
+          </tbody>
+        </table>
+      </div>
     )
 
   }
